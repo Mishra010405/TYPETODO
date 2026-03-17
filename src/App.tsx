1c8cd618a529d52c872d66cc0d7420616e02ba32
Shivam Mishra <mshivam2062@gmail.com>
2026-03-17 15:45:55 +0530

    TODO With TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Addtodo from './Components/Addtodo'
+
+function App() {
+ 
+
+  return (
+    <main>
+      <h1>TODO REACT + TYPESCRIPT</h1>
+    <Addtodo/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { TodosProvideer } from "./Store/Todos";
+
+
+import "./index.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <TodosProvideer>
+        <App />
+      </TodosProvideer>
+    </BrowserRouter>
+  </StrictMode>
+);
