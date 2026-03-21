e9d82dc8bc2076475c61a55bfadf1c3badab115f
Shivam Mishra <mshivam2062@gmail.com>
2026-03-21 09:16:24 +0530

    TODO With TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import Todos from './Components/Todos'
 
 function App() {
  
+
+  return (
+    <main>
+      <h1 className="text-5xl font-bold text-center">
+      TODO REACT + TYPESCRIPT
+      </h1>
     <Addtodo/>
     <Todos/>
     </main>
