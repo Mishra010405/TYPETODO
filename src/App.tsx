a07ba62a97bff74426cb1d1ff31cd77ce60fdade
Shivam Mishra <mshivam2062@gmail.com>
2026-03-24 17:54:30 +0530

    TODO With TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,7 @@ function App() {
  
 
   return (
-    <main>
-      <h1 className="text-5xl font-bold text-center">
-      TODO REACT + TYPESCRIPT
-      </h1>
-    <Addtodo/>
-    <Todos/>
+    <m
     </main>
   )
 }
